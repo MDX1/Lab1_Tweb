5d6c3aa6cd67927999229a893829c88bce8a396c
[Caraulan Dumitru] <caraulandumitru@gmail.com>
2022-02-09 09:17:04 +0200

    Lab2 - new React project

---
+++ lab2/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <pre>
+        Numele: Caraulan
+        Prenumele: Dumitru
+        Grupa: CR-191
+      </pre>
+    </div>
+  );
+}
+
+export default App;
